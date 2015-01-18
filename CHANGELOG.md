7f71463058ff01876b151f8f6a1ccb055111301b
Matt Gibson <downrightlies@gmail.com>
2015-01-18 17:31:49 +0000

    Fix metadata and changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,9 +5,6 @@ This file is used to list changes made in each version of the rails_console cook
 
 0.1.0
 -----
-- [your_name] - Initial release of rails_console
+- Matt Gibson - Initial release of rails_console
 
-- - -
-Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
 
-The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
-name             'rails_console'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures rails_console'
+name             'OpsWorks Rails Command Line'
+maintainer       'Matt Gibson'
+maintainer_email 'downrightlies@gmail.com'
+license          'MIT'
+description      'Installs/Configures Rake tasks so that the Rails console and other Rake tasks work from the command line of OpsWorks'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
